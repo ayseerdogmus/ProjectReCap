29be00e567bd2ae0b59d9d8a044374602cf50642
ayseebiclioglu <ayseebiclioglu@gmail.com>
2021-01-31 18:53:41 +0300

    ReCapProject

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        //Bir iş sınıfı başka sınıfları new lemez!!!
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+            new Car{CarId=1,BrandId=1,ColorId=6,DailyPrice=500,Description="Orta Segment",ModelYear=2010},
+            new Car{CarId=2,BrandId=1,ColorId=2,DailyPrice=1500,Description="Yüksek Segment",ModelYear=2020},
+            new Car{CarId=3,BrandId=2,ColorId=1,DailyPrice=1800,Description="Yüksek Segment",ModelYear=2020},
+            new Car{CarId=4,BrandId=3,ColorId=3,DailyPrice=100,Description="Düşük Segment",ModelYear=2001},
+            new Car{CarId=5,BrandId=3,ColorId=16,DailyPrice=700,Description="Orta Segment",ModelYear=2018}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete=null;
+
+            //LINQ
+            carToDelete = _cars.SingleOrDefault(c=>c.CarId==car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = null;
+            //LINQ
+            carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
